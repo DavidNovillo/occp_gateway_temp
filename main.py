e11f3706299a523d4912a6adad30d7f7be8bb244
David <davidnovillo90@gmail.com>
2024-07-10 20:38:32 -0500

    cambio en las rutas del archivo main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import subprocess
 
 def main():
     subprocess.call(["git", "pull", "origin", "main"],
-                    cwd='/home/pi/OCPP/ocpp_gateway')
+                    cwd='/home/pi/OCPP_TEMP/ocpp_gateway_temp')
     try:
         subprocess.call(["python", "programa_principal.py"],
-                        cwd='/home/pi/OCPP/ocpp_gateway')
+                        cwd='/home/pi/OCPP_TEMP/ocpp_gateway_temp')
     except:
         print("Programa terminado")
 
